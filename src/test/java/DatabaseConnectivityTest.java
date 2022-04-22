d1385c39f9c848eee7e3550e4134c5bc852f8599
AdeshMask <adeshmaske255@gmail.com>
2022-04-22 20:00:11 +0530

    Added:Search by name

--- src/test/java/DatabaseConnectivityTest.java
+++ src/test/java/DatabaseConnectivityTest.java
@@ -1,16 +1,28 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.SQLException;
 
 import static org.junit.Assert.*;
 
 public class DatabaseConnectivityTest {
-
+    DatabaseConnectivity databaseConnectivity = new DatabaseConnectivity();
     @Test
-    public void main() {
+    public void testUpdateQuery() throws SQLException {
+        DatabaseConnectivity databaseConnectivity = new DatabaseConnectivity();
+
+//        String expected = databaseConnectivity.updateQuery();
+//        Assert.assertEquals("3000000",expected);
 
     }
 
     @Test
-    public void listDrivers() {
+    public void testListDrivers() {
+
+    }
+
+    @Test
+    public void showRecord() {
 
     }
 }
